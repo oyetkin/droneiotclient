f1bc979bc87df7f7eb7464c9f5b02fdb48e26234
amtambe <arjuntambe1@gmail.com>
2021-03-08 13:44:05 -0800

    visualize sensor location from json

--- visualize_sensors.py
+++ visualize_sensors.py
@@ -25,21 +25,27 @@ bbox = (IMAGE_LEFT, IMAGE_RIGHT, IMAGE_BOT, IMAGE_TOP)
 
 GET_URL = "https://api.is-conic.com/api/v0p1/debug/get_data"
 
-def visualize_geography():
-    """
-    Show all sensors as points on a USA map
-    """
-    #read into pandas --> isna --> filter lat/lon
-    x = [-117.084493]
-    y = [32.638612]
+json_data = '{"data":[{"timestamp":128,"key":"Arjun_weather_kit","unit":"RF%","value":57.0,"lat":35.12,"lon":-121.84},{"timestamp":130,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":130,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":132,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":133,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":135,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":135,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":138,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":138,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":140,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":140,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":143,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":143,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":145,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":145,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":148,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":148,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":150,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":150,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":153,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":153,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":155,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":155,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":158,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":158,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":160,"key":"Arjun_weather_kit","unit":"Celsius","value":24.6,"lat":35.12,"lon":-121.84},{"timestamp":160,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84},{"timestamp":163,"key":"Arjun_weather_kit","unit":"Celsius","value":24.7,"lat":35.12,"lon":-121.84},{"timestamp":163,"key":"Arjun_weather_kit","unit":"RF%","value":56.0,"lat":35.12,"lon":-121.84}]}'
 
-    background_map = plt.imread(os.getcwd() + "/usa_map.png")
+def visualize_geography(df):
+    """
+    Show all sensors as points on a USA map; label their names
+    """
+    #take any location with more than 1 reading
+    df = df.drop_duplicates(subset=['lat', 'lon'])
+
     fig, ax = plt.subplots()
-    #ax.set_title('Plotting Spatial Data on the Oval')
-    ax.scatter(x, y, zorder=1, alpha= 0.7, c='C1')
+    #plot the points and annotate the station id
+    ax.scatter('lon', 'lat', zorder=1, alpha= 0.7, c='C1', data=df)
+    for i in range(len(df)):
+        ax.annotate(df.iloc[i]['key'], (df.iloc[i]['lon'], df.iloc[i]['lat']))
+    #load in the map image in the background
+    background_map = plt.imread(os.getcwd() + "/usa_map.png")
+    ax.imshow(background_map, zorder=0, extent = bbox, aspect= 'equal')
+    #labels and bounds
+    ax.set_title('Location of All Sensors')
     ax.set_xlim(IMAGE_LEFT, IMAGE_RIGHT)
     ax.set_ylim(IMAGE_BOT, IMAGE_TOP)
-    ax.imshow(background_map, zorder=0, extent = bbox, aspect= 'equal')
     plt.show()
 
 def visualize_graphs():
@@ -51,11 +57,13 @@ def visualize_graphs():
 
 
 if __name__ == "__main__":
+    """
     header = {"Content-Type": "application/json"}
     response = requests.request("GET", GET_URL, headers=header)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     #response.json()
-
-    #visualize_geography()
+    """
+    df = pd.DataFrame.from_dict(json.loads(json_data)['data'])
+    visualize_geography(df)
     #visualize_graphs()
